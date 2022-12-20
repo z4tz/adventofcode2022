00520a8f85185f870c746bbed6cc90b53f96666c
jimmal <jimmy.malmqvist@maxiv.lu.se>
2022-12-20 16:10:15 +0100

    day 16, improved solution

--- day16.py
+++ day16.py
@@ -1,8 +1,9 @@
 import os
+from collections import defaultdict
 from inputreader import aocinput
 import re
 from dataclasses import dataclass
-from itertools import permutations
+
 
 @dataclass
 class Valve:
@@ -12,11 +13,7 @@ class Valve:
     access_times: dict = None
 
 
-results = []
-results2 = []
-
-
-def release_pressure(data: list[str]) -> int:
+def release_pressure(data: list[str]) -> tuple[int, int]:
     valves = {}
     regex = re.compile(r"^Valve (..) has flow rate=(\d*); tunnels* leads* to valves* ([A-Z, ]*)$")
     for line in data:
@@ -40,48 +37,32 @@ def release_pressure(data: list[str]) -> int:
             steps += 1
         valves[valve_name].access_times = shortest
 
-    def action(valve: Valve, time_remaining: int, pressure: int, closed_valves: set[str]):
-        if time_remaining <= 0 or len(closed_valves) == 0:
-            results.append(pressure)
-            return
+    relevant_closed_valves = [valve for valve in valves.keys() if valves[valve].flow_rate > 0]
+
+    def action(valve: Valve, time_remaining: int, pressure: int, closed_valves: set[str], open_valves: frozenset[str]):
+        states[open_valves] = max([pressure, states[open_valves]])
         for next_valve in closed_valves:
-            new_closed = closed_valves.copy()
-            new_closed.remove(next_valve)
-            action(valves[next_valve], time_remaining - valve.access_times[next_valve], pressure + valves[next_valve].flow_rate * (time_remaining-valve.access_times[next_valve]), new_closed)
-
-    def elephant_action(valve1, valve2, time1: int, time2: int, pressure: int, closed_valves: set[str]):
-        if len(closed_valves) == 0 or (time1 <=0 and time2 <= 0):
-            #print(time1, time2)
-            results2.append(pressure)
-            return
-        elif len(closed_valves) == 1:
-            next_valve = closed_valves.pop()
-            pressure_1 = valves[next_valve].flow_rate * (time1-valve1.access_times[next_valve])
-            pressure_2 = valves[next_valve].flow_rate * (time2-valve2.access_times[next_valve])
-            if pressure_1 > pressure_2:
-                elephant_action(valves[next_valve], valve2, time1 - valve1.access_times[next_valve], time2, pressure + pressure_1, set())
-            else:
-                elephant_action(valve1, valves[next_valve], time1, time2 - valve2.access_times[next_valve], pressure + pressure_2, set())
-        else:
-            for next1, next2 in permutations(closed_valves, 2):
-                if time1-valve1.access_times[next1] < 0 or time2-valve2.access_times[next2] < 0:
-                    continue
+            new_time = time_remaining - valve.access_times[next_valve]
+            if new_time >= 0:
                 new_closed = closed_valves.copy()
-                new_closed.remove(next1)
-                new_closed.remove(next2)
-                new_pressure = pressure + valves[next1].flow_rate * (time1-valve1.access_times[next1])
-                new_pressure = new_pressure + valves[next2].flow_rate * (time2-valve2.access_times[next2])
-                elephant_action(valves[next1], valves[next2], time1 - valve1.access_times[next1], time2 - valve2.access_times[next2], new_pressure,new_closed)
+                new_closed.remove(next_valve)
+                new_open = frozenset([*open_valves, next_valve])
+                action(valves[next_valve], new_time, pressure + valves[next_valve].flow_rate * new_time, new_closed, new_open)
+    # part 1
+    states = defaultdict(int)
+    action(valves['AA'], 30, 0, set(relevant_closed_valves), frozenset())
+    result = max(states.values())
 
+    # part 2
+    states = defaultdict(int)
+    action(valves['AA'], 26, 0, set(relevant_closed_valves), frozenset())
+    max_score = 0
+    for state1 in states.items():  # find the two states with max score that didn't open the same valve
+        for state2 in states.items():
+            if (score := state1[1] + state2[1]) > max_score and state1[0].isdisjoint(state2[0]):
+                max_score = score
 
-
-
-    relevant_closed_valves = [valve for valve in valves.keys() if valves[valve].flow_rate>0]
-    #action(valves['AA'], 30, 0, set(relevant_closed_valves))
-    #print(max(results))
-    elephant_action(valves['AA'], valves['AA'], 26, 26, 0, set(relevant_closed_valves))
-    print(max(results2))
-
+    return result, max_score
 
 
 def main(day):
